4c7bb6a1ff1211e285a1cfeb44f0310acbac2181
WatIsDeze <watisdeze2@gmail.com>
2021-06-28 11:38:53 +0200

    Renamed "maxClients" cvar back to "maxclients" as it should be :)

--- src/svgame/main.cpp
+++ src/svgame/main.cpp
@@ -149,7 +149,7 @@ void SVG_InitGame(void)
     gi.cvar("gamename", GAMEVERSION , CVAR_SERVERINFO | CVAR_LATCH);
     gi.cvar("gamedate", __DATE__ , CVAR_SERVERINFO | CVAR_LATCH);
 
-    maxClients = gi.cvar("maxClients", "4", CVAR_SERVERINFO | CVAR_LATCH);
+    maxClients = gi.cvar(maxclients, "4", CVAR_SERVERINFO | CVAR_LATCH);
     maxspectators = gi.cvar("maxspectators", "4", CVAR_SERVERINFO);
     deathmatch = gi.cvar("deathmatch", "0", CVAR_LATCH);
     coop = gi.cvar("coop", "0", CVAR_LATCH);
