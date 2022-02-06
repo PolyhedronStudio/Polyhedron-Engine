495080d8c1bb463ef6f9d8a5c458ad20e2db2c2e
WatIsDeze <10252874+WatIsDeze@users.noreply.github.com>
2022-02-06 16:06:22 +0100

    More cleaning up/styling for src/Client/Entities.cpp

--- src/Client/Entities.cpp
+++ src/Client/Entities.cpp
@@ -76,35 +76,35 @@ static inline void Entity_UpdateNew(ClientEntity *ent, const EntityState *state,
 /**
 *   @brief  Updates an existing entity using the newly received state for it.
 **/
-static inline void Entity_UpdateExisting(ClientEntity *ent, const EntityState *state, const vec_t *origin)
+static inline void Entity_UpdateExisting(ClientEntity *clientEntity, const EntityState *state, const vec_t *origin)
 {
     // Fetch event ID.
     int32_t eventID = state->eventID;
 
-    if (state->modelIndex != ent->current.modelIndex
-        || state->modelIndex2 != ent->current.modelIndex2
-        || state->modelIndex3 != ent->current.modelIndex3
-        || state->modelIndex4 != ent->current.modelIndex4
+    if (state->modelIndex != clientEntity->current.modelIndex
+        || state->modelIndex2 != clientEntity->current.modelIndex2
+        || state->modelIndex3 != clientEntity->current.modelIndex3
+        || state->modelIndex4 != clientEntity->current.modelIndex4
         || eventID == EntityEvent::PlayerTeleport
         || eventID == EntityEvent::OtherTeleport
-        || fabsf(origin[0] - ent->current.origin[0]) > 512
-        || fabsf(origin[1] - ent->current.origin[1]) > 512
-        || fabsf(origin[2] - ent->current.origin[2]) > 512
+        || fabsf(origin[0] - clientEntity->current.origin[0]) > 512
+        || fabsf(origin[1] - clientEntity->current.origin[1]) > 512
+        || fabsf(origin[2] - clientEntity->current.origin[2]) > 512
         || cl_nolerp->integer == 1) 
     {
         // Some data changes will force no lerping.
-        ent->trailcount = 1024;     // Used for diminishing rocket / grenade trails
+        clientEntity->trailcount = 1024;     // Used for diminishing rocket / grenade trails
 
         // Duplicate the current state so lerping doesn't hurt anything
-        ent->prev = *state;
+        clientEntity->prev = *state;
 
         // No lerping if teleported or morphed
-        ent->lerpOrigin = origin;
+        clientEntity->lerpOrigin = origin;
         return;
     }
 
     // Shuffle the last state to previous
-    ent->prev = ent->current;
+    clientEntity->prev = clientEntity->current;
 }
 
 /**
@@ -113,12 +113,12 @@ static inline void Entity_UpdateExisting(ClientEntity *ent, const EntityState *s
 *   @return True if it has not been around in the previous frame. False if it
 *           has not been around in previous frames. 
 **/
-static inline qboolean Entity_IsNew(const ClientEntity *ent)
+static inline qboolean Entity_IsNew(const ClientEntity *clientEntity)
 {
     if (!cl.oldframe.valid)
         return true;   // Last received frame was invalid.
 
-    if (ent->serverFrame != cl.oldframe.number)
+    if (clientEntity->serverFrame != cl.oldframe.number)
         return true;   // Wasn't in last received frame.
 
     if (cl_nolerp->integer == 2)
@@ -137,99 +137,161 @@ static inline qboolean Entity_IsNew(const ClientEntity *ent)
 *   @brief  Updates the entity belonging to the entity state. If it doesn't
 *           exist yet, it'll create it.
 **/
-static void entity_update(const EntityState *state)
+static void Entity_UpdateState(const EntityState *state)
 {
-    ClientEntity *ent = &cs.entities[state->number];
-    const vec_t *origin;
-    vec3_t origin_v;
+    ClientEntity *clientEntity = &cs.entities[state->number];
+    vec3_t entityOrigin = vec3_zero();
 
     // If entity its solid is PACKED_BSP, decode mins/maxs and add to the list
     if (state->solid && state->number != cl.frame.clientNumber + 1
         && cl.numSolidEntities < MAX_PACKET_ENTITIES) {
-        cl.solidEntities[cl.numSolidEntities++] = ent;
+        cl.solidEntities[cl.numSolidEntities++] = clientEntity;
 
         if (state->solid != PACKED_BSP) {
             // 32 bit encoded bbox
-            MSG_UnpackBoundingBox32(state->solid, ent->mins, ent->maxs);
+            MSG_UnpackBoundingBox32(state->solid, clientEntity->mins, clientEntity->maxs);
         }
     }
 
-    // work around Q2PRO server bandwidth optimization
+    // Work around Q2PRO server bandwidth optimization.
     const bool isOptimizedEntity = Entity_IsOptimized(state);
 
     if (isOptimizedEntity) {
-        origin = origin_v = cl.frame.playerState.pmove.origin;
+        entityOrigin = cl.frame.playerState.pmove.origin;
     } else {
-        origin = state->origin;
+        entityOrigin = state->origin;
     }
 
-    if (Entity_IsNew(ent)) {
-        // wasn't in last update, so initialize some things
-        Entity_UpdateNew(ent, state, origin);
+    if (Entity_IsNew(clientEntity)) {
+        // Wasn't in last update, so initialize some things.
+        Entity_UpdateNew(clientEntity, state, entityOrigin);
     } else {
-        Entity_UpdateExisting(ent, state, origin);
+        Entity_UpdateExisting(clientEntity, state, entityOrigin);
     }
 
-    ent->serverFrame = cl.frame.number;
-    ent->current = *state;
+    clientEntity->serverFrame = cl.frame.number;
+    clientEntity->current = *state;
 
     // work around Q2PRO server bandwidth optimization
     if (isOptimizedEntity) {
-        Com_PlayerToEntityState(&cl.frame.playerState, &ent->current);
+        Com_PlayerToEntityState(&cl.frame.playerState, &clientEntity->current);
     }
 }
 
 /**
 *   @brief  Notifies the client game about an entity event to execute.
 **/
-static void Entity_ExecuteEvent(int number)
-{
-    // N&C: Let the CG Module handle this.
+static void Entity_FireEvent(int number) {
     CL_GM_EntityEvent(number);
 }
 
-static void set_active_state(void)
+/**
+*   @brief  Updates the player's state between previous and current frame.
+**/
+static void Player_UpdateStates(ServerFrame *previousFrame, ServerFrame *currentFrame, int framediv)
 {
+    PlayerState *currentPlayerState = nullptr, *previousPlayerState = nullptr;
+    ClientEntity *clientEntity = nullptr;
+    int32_t previousFrameNumber = 0;
+
+    // Fetch states to interpolate between.
+    currentPlayerState  = &currentFrame->playerState;
+    previousPlayerState = &previousFrame->playerState;
+
+    // No lerping if previous frame was dropped or invalid
+    if (!previousFrame->valid)
+        goto duplicate;
+
+    // If the previous frame number we expected does not add up to the one we got
+    // passed to this function, go to duplicate.
+    previousFrameNumber = currentFrame->number - framediv;
+    if (previousFrame->number != previousFrameNumber)
+	goto duplicate;
+
+    // No lerping if player entity was teleported (origin check).
+    if (std::fabsf((float)(previousPlayerState->pmove.origin[0] - currentPlayerState->pmove.origin[0])) > 256 ||
+        std::fabsf((float)(previousPlayerState->pmove.origin[1] - currentPlayerState->pmove.origin[1])) > 256 ||
+        std::fabsf((float)(previousPlayerState->pmove.origin[2] - currentPlayerState->pmove.origin[2])) > 256) {
+        goto duplicate;
+    }
+    // no lerping if player entity was teleported (event check)
+    clientEntity = &cs.entities[currentFrame->clientNumber + 1];
+    if (clientEntity->serverFrame > previousFrameNumber && clientEntity->serverFrame <= currentFrame->number && 
+       (clientEntity->current.eventID == EntityEvent::PlayerTeleport || clientEntity->current.eventID == EntityEvent::OtherTeleport)) 
+    {
+        goto duplicate;
+    }
+
+    // No lerping if teleport bit was flipped.
+    if ((previousPlayerState->pmove.flags ^ currentPlayerState->pmove.flags) & PMF_TIME_TELEPORT)
+        goto duplicate;
+    // No lerping if POV number changed.
+    if (previousFrame->clientNumber != currentFrame->clientNumber)
+        goto duplicate;
+    // Developer option.
+    if (cl_nolerp->integer == 1)
+        goto duplicate;
+
+    return;
+
+duplicate:
+    // duplicate the current state so lerping doesn't hurt anything
+    *previousPlayerState= *currentPlayerState;
+}
+
+/**
+*   @brief  Set the client state to active after having precached all data.
+**/
+static void SetActiveState(void) {
+    // Switch to an active connection state.
     cls.connectionState = ClientConnectionState::Active;
 
+    // Calculate server delta.
     cl.serverDelta = Q_align(cl.frame.number, CL_FRAMEDIV);
     cl.time = cl.serverTime = 0; // set time, needed for demos
 
-    // initialize oldframe so lerping doesn't hurt anything
+    // Initialize oldframe so lerping doesn't hurt anything.
     cl.oldframe.valid = false;
     cl.oldframe.playerState = cl.frame.playerState;
     cl.frameFlags = 0;
+    
+    // Update sequences.
     if (cls.netChannel) {
         cl.initialSequence = cls.netChannel->outgoingSequence;
     }
+
     if (cls.demo.playback) {
-        // init some demo things
+        // Initialize first demo frame.
         CL_FirstDemoFrame();
     } else {
-        // set initial cl.predicted_origin and cl.predicted_angles
+        // Set initial cl.predicted_origin and cl.predicted_angles.
         cl.predictedState.viewOrigin = cl.frame.playerState.pmove.origin;
         cl.predictedState.velocity = cl.frame.playerState.pmove.velocity;
 
         if (cl.frame.playerState.pmove.type < EnginePlayerMoveType::Dead) {
-            // enhanced servers don't send viewAngles
-            // N&C: Let the client game module predict angles.
+            // Enhanced servers don't send viewAngles
+            // Let the client game module predict angles.
             CL_GM_PredictAngles();
         } else {
-            // just use what server provided
+            // Just use what server provided.
             cl.predictedState.viewAngles = cl.frame.playerState.pmove.viewAngles;
         }
     }
 
-    SCR_EndLoadingPlaque();     // get rid of loading plaque
+    // Get rid of loading plaque.
+    SCR_EndLoadingPlaque();
     SCR_LagClear();
 
-
-    // Open the menu here iafter we're done loading the map properly.
+    // Open the menu here after we're done loading the map properly.
     CL_OpenBSPMenu();
-    Con_Close(false);          // get rid of connection screen
 
+    // Close the console to get rid of connection screen.
+    Con_Close(false);
+
+    // Check for pauses.
     CL_CheckForPause();
 
+    // Update frame times.
     CL_UpdateFrameTimes();
 
     if (!cls.demo.playback) {
@@ -237,60 +299,6 @@ static void set_active_state(void)
         Cmd_ExecTrigger("#cl_enterlevel");
     }
 }
-
-static void
-player_update(ServerFrame *oldframe, ServerFrame *frame, int framediv)
-{
-    PlayerState *ps, *ops;
-    ClientEntity *ent;
-    int oldnum;
-
-    // find states to interpolate between
-    ps = &frame->playerState;
-    ops = &oldframe->playerState;
-
-    // no lerping if previous frame was dropped or invalid
-    if (!oldframe->valid)
-        goto dup;
-
-    oldnum = frame->number - framediv;
-    if (oldframe->number != oldnum)
-        goto dup;
-
-    // no lerping if player entity was teleported (origin check)
-    // N&C: FF Precision.
-    // CPP: Added fabs instead of abs
-    if (std::fabsf((float)(ops->pmove.origin[0] - ps->pmove.origin[0])) > 256 ||
-        std::fabsf((float)(ops->pmove.origin[1] - ps->pmove.origin[1])) > 256 ||
-        std::fabsf((float)(ops->pmove.origin[2] - ps->pmove.origin[2])) > 256) {
-        goto dup;
-    }
-    // no lerping if player entity was teleported (event check)
-    ent = &cs.entities[frame->clientNumber + 1];
-    if (ent->serverFrame > oldnum &&
-        ent->serverFrame <= frame->number &&
-        (ent->current.eventID == EntityEvent::PlayerTeleport
-         || ent->current.eventID == EntityEvent::OtherTeleport)) {
-        goto dup;
-    }
-
-    // no lerping if teleport bit was flipped
-    if ((ops->pmove.flags ^ ps->pmove.flags) & PMF_TIME_TELEPORT)
-        goto dup;
-    // no lerping if POV number changed
-    if (oldframe->clientNumber != frame->clientNumber)
-        goto dup;
-    // developer option
-    if (cl_nolerp->integer == 1)
-        goto dup;
-
-    return;
-
-dup:
-    // duplicate the current state so lerping doesn't hurt anything
-    *ops = *ps;
-}
-
 /*
 ==================
 CL_DeltaFrame
@@ -300,38 +308,33 @@ A valid frame has been parsed.
 */
 void CL_DeltaFrame(void)
 {
-    ClientEntity    *ent;
-    EntityState     *state;
-    int32_t i, j;
-    int32_t frameNumber;
-    int32_t prevstate = cls.connectionState;
+    // Getting a valid frame message ends the connection process.
+    if (cls.connectionState == ClientConnectionState::Precached) {
+	    SetActiveState();
+    }
 
-    // getting a valid frame message ends the connection process
-    if (cls.connectionState == ClientConnectionState::Precached)
-        set_active_state();
-
-    // set server time
-    frameNumber = cl.frame.number - cl.serverDelta;
+    // Set server time
+    int32_t frameNumber = cl.frame.number - cl.serverDelta;
     cl.serverTime = frameNumber * CL_FRAMETIME;
 
-    // rebuild the list of solid entities for this frame
+    // Rebuild the list of solid entities for this frame
     cl.numSolidEntities = 0;
 
-    // initialize position of the player's own entity from playerstate.
+    // Initialize position of the player's own entity from playerstate.
     // this is needed in situations when player entity is invisible, but
-    // server sends an effect referencing it's origin (such as MuzzleFlashType::Login, etc)
-    ent = &cs.entities[cl.frame.clientNumber + 1];
-    Com_PlayerToEntityState(&cl.frame.playerState, &ent->current);
+    // server sends an effect referencing its origin (such as MuzzleFlashType::Login, etc)
+    ClientEntity *playerClientEntity = &cs.entities[cl.frame.clientNumber + 1];
+    Com_PlayerToEntityState(&cl.frame.playerState, &playerClientEntity->current);
 
-    for (i = 0; i < cl.frame.numEntities; i++) {
-        j = (cl.frame.firstEntity + i) & PARSE_ENTITIES_MASK;
-        state = &cl.entityStates[j];
+    for (int32_t i = 0; i < cl.frame.numEntities; i++) {
+        int32_t stateIndex = (cl.frame.firstEntity + i) & PARSE_ENTITIES_MASK;
+        EntityState *state = &cl.entityStates[stateIndex];
 
-        // set current and prev
-        entity_update(state);
+        // Update the entity state. (Sets current and previous state.)
+        Entity_UpdateState(state);
 
-        // fire events
-        Entity_ExecuteEvent(state->number);
+        // Fire entity event.
+        Entity_FireEvent(state->number);
     }
 
     if (cls.demo.recording && !cls.demo.paused && !cls.demo.seeking && CL_FRAMESYNC()) {
@@ -348,12 +351,14 @@ void CL_DeltaFrame(void)
         IN_Activate();
     }
 
-    player_update(&cl.oldframe, &cl.frame, 1);
+    // Update player state between previous and current frame.
+    Player_UpdateStates(&cl.oldframe, &cl.frame, 1);
 
+    // Check for prediction errors.
     CL_CheckPredictionError();
 
+    // Call into client game its delta frame function.
     CL_GM_ClientDeltaFrame();
-    //SCR_SetCrosshairColor();
 }
 
 #ifdef _DEBUG
