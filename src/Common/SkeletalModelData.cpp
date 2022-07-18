4c0bd11534803224c8dd9ffbb5cf5acf9c2156cd
WatIsDeze <10252874+WatIsDeze@users.noreply.github.com>
2022-07-18 20:26:45 +0200

    Properly tokenizes our ".skc" text configuration files. Next up, readjusting the animation system to work with those and then we're quite close to finishing the basic skeleton work. Epic winning!

--- src/Common/SkeletalModelData.cpp
+++ src/Common/SkeletalModelData.cpp
@@ -72,7 +72,7 @@ void SKM_GenerateModelData(model_t* model) {
 	// Get our Joint data sorted out nicely.
 	for (int32_t jointIndex = 0; jointIndex < model->iqmData->num_joints; jointIndex++) {
 		// Get the parent joints.
-		const const int32_t jointParentIndex = (jointIndex == 0 ? -1 : model->iqmData->jointParents[jointIndex]);
+		const int32_t jointParentIndex = (jointIndex == 0 ? -1 : model->iqmData->jointParents[jointIndex]);
 
 		// Create our joint object.
 		SkeletalModelData::Joint jointData = {
@@ -299,37 +299,61 @@ void SKM_GenerateModelData(model_t* model) {
 *
 *	Animation Configuration Parsing:
 *	
-*	In order to properly deal with skeletal model file formats in a game code friendly way,
-*	we want to parse additional animation config files. These files can be used to configurate
-*	the following:
-*	- Animation "Actions", setting the properties of:
-*		[name]:		The name of this animation "action", used for indexing.
-*		[start]:	The first animation frame in the long iqm frame sequence range.
-*		[end]:		The last, final, end animation frame in the long iqm frame sequence range.
-*		[loopcount]:The loopcount, if 0 it only plays the animation once and stops at the last
-*					frame. It only replays itself if requested by the Game code.
-*		[frametime]:Amount of time each frame takes for this animation.
-*	- Tags which can be named and assigned to a bone. Each tag allows for specifying the following
-*	properties:
-*			[xOffset,yOffset,zOffset]: The offset relative to the bone's position.
-*			[ rotation offset? ]:
-*			[ rotation constraints ? ]:
-*	- Animation "Blends", each animation is composed of animation blends. Using animation blends
-*	it is possible to compose unique animations that combine multiple actions starting from a 
-*	specified bone node. The bone node can be indexed by number, as well as by name.
+*	Takes care of parsing animation configuration files such as this example:
 *
-*	To compose an animation blend works like the following example:
-*	animation "walking_"
-*		blend "standard_walk" 1.0
-*		blend "reload
-*			
-*	
+*	----
+*	rootbone "mixamo8:Spine" 
 *
+*	headbone "mixamo8:neckthing" 
+*	torsobone "mixamo8:Spine1" 
+*
+*	action "idle" 0 1 -1 16.66 
+*	action "run" 23 64 -1 16.66 
+*	action "reload" 65 95 -1 16.66 
+*
+*	animation "RunAndReload" 
+*		blendaction "run" 1.0 
+*		blendaction "reload" 1.0 torsobone 
+*	----
+*
+*	The following 'commands' are available:
+*	'rootbone': Specifies which bone acts as a rootmotion movement origin point.
+*				Can be identified by a string name, or integral number index.
+*
+*	'headbone':	Specifies where the head starts in case of a humanoid model.
+*				Can be identified by a string name, or integral number index.
+*	'torsobone':Specifies where the torso starts in case of a humanoid model.
+*				Can be identified by a string name, or integral number index.
+*
+*	'action':	Actions define specific animation frame sets by assigning them
+*				a unique distinct name, startframe, endframe, loopCount, and frameTime.
+*
+*	'animation':Animations are composed by packing 'blendaction' commands together. 
+*				The first blend 'takes the lead', and is considered the dominating
+*				animation on which we blend other animations into.
+*				
+*	'blendaction':	Assigns an 'action' to a bone node with the given blend fraction.
+*					If no bonenode is present, it is regarded as the leading dominator
+*					animation instead.
+*				
 *
 *
 **/
 //! Contains a parsed token.
 struct SKMParsedToken {
+	//! Token Flags.
+	struct Flags {
+		//! This flag is set whenever a an opening double quote has matched an ending double quote.
+		static constexpr int32_t QuotedString = 1 << 1;
+		//! This flag is set when the token passed an integral number test.
+		static constexpr int32_t IntegralNumber = 1 << 2;
+		//! This flag is set when the token passed a floating point number test.
+		static constexpr int32_t FloatNumber = 1 << 3;
+		//! This flag is set if the token is contained within our commands list.
+		static constexpr int32_t CommandIdentifier = 1 << 4;
+		//! This flag is set when all of the above have failed, we'll assume that it is an identifier of sorts.
+		static constexpr int32_t Identifier = 1 << 5;
+	};
 	//! Line number we found this token at.
 	int32_t line = 0;
 	//! Character offset number into our line at which we found this token at.
@@ -344,100 +368,267 @@ struct SKMParsedToken {
 						// In case of cmd, also a cmd index is set.
 };
 
+//! Stores the final parsed results of each line.
+struct SKMParsedLine {
+	//! Actual line number.
+	int32_t number = 0;
+	
+	//! Total character count (width) of the line.
+	int32_t width = 0;
+
+	//! String value containing line data.
+	std::string value = "";
+
+	//! Filled with 'categorized' type tokens found in the line's string value.
+	std::vector<SKMParsedToken> tokens;
+};
+
 //! Contains our parsed tokens.
 struct SKMParsedResults {
 	std::vector<SKMParsedToken> tokens;
 };
 
-static SKMParsedResults skmParsedResults;
+//! String list of all our existing commands.
+static std::vector< std::string > skmCommandList = {
+	"rootbone",
+	"headbone",
+	"torsobone",
+	"action",
+	"animation",
+	"blendaction",
+};
+
+
+///*********
+//*	Temporary: TODO: Move to string header? Maybe make more utilities, look into the tokenizing process and C++-ify that.
+//**/
+#include <regex>
+// This regex splitter does not seem to work, unsure if it is our fault or MS.
+static inline std::vector<std::string> RegExSplitter(std::string input) {
+    std::regex output("(? : ^|s *)(S + )");
+	//"^[ |\t|\n|\r|\v|\f]*|[ |\t|\n|\r|\v|\f]*$" 
+    return { std::sregex_token_iterator(std::cbegin(input), std::cend(input), output, 1), std::sregex_token_iterator() };
+}
+static inline const std::string StringRegex( const std::string &str, const std::string &regularExpression = "" ) {
+	return std::regex_replace(str, std::regex(regularExpression), "");
+}
+static inline const std::string StringRegexTrim( const std::string &str, const std::string &regularExpression = "^[ |\t|\n|\r|\v|\f]*|[ |\t|\n|\r|\v|\f]*$" ) {
+	return StringRegex( str, regularExpression );
+}
+//
+///**
+//*	End of Temporary.
+//*********/
+
+
+
 
 /**
-*	@brief	Recursively tokenizes a buffer by spaces, and double quotes.
+*	@return	The flag(s) categorizing(describing the intent) of this token's string content.
 **/
-static SKMParsedToken SKM_TokenizeStringSegment( const std::string &segment, const int32_t line = 0, const int32_t offset = 0, const int32_t width = 0 ) {
-	
+static int32_t SKM_CategorizeTokenValue( const std::string &tokenValue ) {
 	// Flags that tell us what the actual content of this token is.
-	int32_t flags = 0;
+	int32_t flagsMask = 0;
 
-	Com_DPrintf("token [%s, line:%i, offset:%i, width:%i, flags:%s]\n",
-				segment.c_str(),
-				line,
-				offset,
-				width,
-				"");
+	/**
+	*	#0: Test whether this token is any of our accepted commands, if it is, skip any other tests.
+	**/
+	// Test whether it is in our commands list.
+	const bool isCommand = std::any_of(skmCommandList.begin(), skmCommandList.begin() + skmCommandList.size(), [&tokenValue](const std::string &cmd) {
+			return cmd == tokenValue; 
+		} 
+	);
 
-	// Create our return token by setting it up with our parsed data.
-	return SKMParsedToken{
-		.line = line,
-		.offset = offset,
-		.width = width,
-		.value = segment,
-		.flags = flags,
-	};
+	/**
+	*	#1: Proceed with other tests if not a command, otherwise apply CommandIdentifier flag.
+	**/
+	// If it is a command, set command identifier flag and skip other tests.
+	if ( isCommand ) {
+		flagsMask |= SKMParsedToken::Flags::CommandIdentifier;
+	// It was no command, proceed for further testing.
+	} else {
+		// Test for strictly integral number.
+		if ( tokenValue.find_first_not_of("-0123456789") == std::string::npos ) {		
+			flagsMask |= SKMParsedToken::Flags::IntegralNumber;
+		// Test for the '.', if it is there, we expect a floating point number.
+		} else if ( tokenValue.find_first_not_of(".-0123456789") == std::string::npos ) {
+			flagsMask |= SKMParsedToken::Flags::FloatNumber;
+		// It's none of the above, so we assume it is an unnamed identifier.
+		} else {
+			flagsMask |= SKMParsedToken::Flags::Identifier;
+		}
+	}
+
+	// Return flagsMask.
+	return flagsMask;
+}
+
+/**
+*	@brief	Tokenizes the SKMParsedLine its value and 
+**/
+static bool SKM_TokenizeParsedLine( SKMParsedLine &parsedLine, std::string &errorString ) {
+	/**
+	*	
+	**/
+    std::size_t previousPosition = 0;
+    std::size_t currentPosition = 0;
+ 
+	while ((currentPosition = parsedLine.value.find_first_of(" \t", previousPosition)) != std::string::npos) {
+        if ( currentPosition > previousPosition ) {
+			// Calculate offset.
+			const int32_t tokenOffset = previousPosition;
+			// Calculate width.
+			const int32_t tokenWidth = currentPosition - previousPosition;
+			// Take token value.
+			const std::string tokenValue = parsedLine.value.substr( tokenOffset, tokenWidth );
+			// Determine special token flags for this token.
+			const int32_t tokenFlags = SKM_CategorizeTokenValue( tokenValue );
+
+			parsedLine.tokens.emplace_back( SKMParsedToken{
+				.line = parsedLine.number,
+				.offset = tokenOffset,
+				.width = tokenWidth,
+				.value = tokenValue,
+				.flags = tokenFlags
+			});
+        }
+        previousPosition = currentPosition + 1;
+    }
+
+	// Take care of the last but of string value.
+	if ( previousPosition < parsedLine.value.length() ) {
+		// Calculate offset.
+		const int32_t tokenOffset = previousPosition;
+		// Calculate width.
+		const int32_t tokenWidth = parsedLine.value.length() - tokenOffset;
+		// Take token value.
+		const std::string tokenValue = parsedLine.value.substr( tokenOffset );
+		// Determine special token flags for this token.
+		const int32_t tokenFlags = SKM_CategorizeTokenValue( tokenValue );
+
+        parsedLine.tokens.emplace_back( SKMParsedToken{
+			.line = parsedLine.number,
+			.offset = tokenOffset,
+			.width = tokenWidth,
+			.value = tokenValue,
+			.flags = tokenFlags
+		});
+    }
+
+	return true;
+}
+
+/**
+*	@brief	Splits the buffer into separate distinct lines. Feeding them to 'parsedLines'.
+*	@param	buffer The buffer to scan and split up into separate distinct lines.
+*	@param	&lineTokens	A vector reference to push back all parsed lines on to.
+*	@return	The total amount of lines found in 'buffer'.
+**/
+static int32_t SKM_SplitLines( const std::string &buffer, std::vector<SKMParsedLine> &parsedLines) {
+	//! Keeps track of total lines found.
+	int32_t totalLineCount = 0;
+
+    std::size_t previousPosition = 0;
+    std::size_t currentPosition = 0;
+ 
+	while ((currentPosition = buffer.find_first_of( "\r\n", previousPosition)) != std::string::npos) {
+        if ( currentPosition > previousPosition ) {
+			// Actual size of line to be pushed.
+			const int32_t lineSize = currentPosition - previousPosition;
+
+			// Emplace our parsed line data.
+			parsedLines.emplace_back(SKMParsedLine{
+				.number = totalLineCount,
+				.width = lineSize,
+				// Trim the substring from all things that cause white spaces, on both left and right sides.
+				.value = StringRegexTrim(
+					buffer.substr( previousPosition, currentPosition - previousPosition )
+				)
+			});
+
+			// Increment line count.
+			totalLineCount++;
+        }
+        previousPosition = currentPosition + 1;
+    }
+
+    if ( previousPosition < buffer.length() ) {
+		// Actual size of line to be pushed.
+		const int32_t lineSize = previousPosition;
+
+		// Emplace our parsed line data.
+		parsedLines.emplace_back(SKMParsedLine{
+			.number = totalLineCount,
+			.width = lineSize,
+			// Trim the substring from all things that cause white spaces, on both left and right sides.
+			.value = StringRegexTrim(
+				buffer.substr( previousPosition )
+			)
+		});
+
+
+		// Increment line count.
+		totalLineCount++;
+    }
+
+	// Return total lines found.
+	return totalLineCount;
 }
 
 /**
 *	@brief	Parsed a Skeletal Model Configuration file for animation actions, tag and blend data.
 **/
 static bool SKM_ParseConfiguration( const std::string &cfgBuffer ) {
+	// Our parsed line container which we first parse all lines, then
+	// tokenize each line and store its tokens.
+	std::vector<SKMParsedLine> parsedLines;
+
 	/**
-	*	#0:	Tokenize our buffer.
+	*	#0:	First split by '\r\n' feeds.
 	**/
-	// Keeps score of the amount of double quotes found. If the number returns 0 for % 2,
-	// it means we've found a matching closing bracket for the specific segment.
-	int32_t doubleQuoteCounter = 0;
-	// Keeps score of the line number.
-	int32_t segmentLine = 0;
-	// Stores our segment string value.
-	std::string segment;
-	// Create a string stream of our configuration string buffer.
-	std::stringstream stream_input(cfgBuffer);
+	int32_t totalLinesParsed = SKM_SplitLines( cfgBuffer, parsedLines );
 
-	// Debug output for testing tokenizing.
-	Com_DPrintf("===================================================\n");
+	/**
+	*	#1: Now actually tokenize each line, storing the tokens in the parsed line.
+	**/
+	for ( auto &parsedLine : parsedLines ) {
+		// This string is set in case of any errors.
+		std::string errorString = "";
 
-	// Start tokenizing by double quotes first.
-	while( std::getline(stream_input, segment, '\"') ) {
-		// Increment our double quote counter.
-		++doubleQuoteCounter;
-
-		// If % 2 == 0 then we've found a matching closing bracket.
-		if ( doubleQuoteCounter % 2 == 0 ) {
-			// If the segment isn't empty, add it to our parsed results.
-			if ( !segment.empty() ) {
-				SKM_TokenizeStringSegment( segment );
-				//std::cout << segment << std::endl;
-			}
-		} else {
-			// Create a string stream of our segment itself.
-			std::stringstream stream_segment(segment);
-
-			// The total string size, used to calculate the token segment offset with.
-			size_t segmentLength = segment.length();
-			// Stores the actual current segment offset.
-			size_t segmentOffset = 0;
-			// Tokenize it by spaces.
-			while( std::getline(stream_segment, segment, ' ') ) {
-				// Only consider the segment a legit token if it contains anything of value.
-				if ( !segment.empty() ) {
-					// Our offset is calculated by subtracting the current stream_segment character count.
-					const int32_t subtractedCharacterCount = stream_segment.gcount();
-					segmentOffset += segmentLength - subtractedCharacterCount;
-
-					// Our segment width is of course, the length of our segment string.
-					const int32_t segmentWidth = segment.length();
-
-					// This works 
-					SKM_TokenizeStringSegment( segment, segmentLine, segmentOffset, segmentWidth );
-					//std::cout << segment << std::endl;
-				}
-			}
-			
-			// Increment line.
-			segmentLine++;
+		// Tokenize the current line.
+		if ( !SKM_TokenizeParsedLine( parsedLine, errorString ) ) {
+			// Something went wrong, output error.
 		}
 	}
+	// Debug output for testing tokenizing.
+	Com_DPrintf("===================================================\n");
+	Com_DPrintf("Tokenizing into lines, and each line into tokens based on spaces and tabs:\n");
+	// Iterate our tokenized line buffers.
+	for (auto &parsedLine : parsedLines ) {
+	//	// Tokenize this line for spaces and tabs.
+	//	auto lineTokens = SKM_StringTokenize( bufferLine, " \t" );
 
+	//	// Print line:
+		Com_DPrintf( "#%i:%s: {\n", parsedLine.number, parsedLine.value.c_str() );
+		for ( auto &token : parsedLine.tokens ) {
+
+			std::string flagStr = "";
+
+			// Finish generating our debug flag mask by testing and adding string counterparts for all remaining 
+			// flags other than the QuoteString flag.
+			if ( token.flags & SKMParsedToken::Flags::CommandIdentifier )	{ flagStr += "CommandIdentifier "; }
+			if ( token.flags & SKMParsedToken::Flags::FloatNumber )			{ flagStr += "FloatNumber "; }
+			if ( token.flags & SKMParsedToken::Flags::IntegralNumber )		{ flagStr += "IntegralNumber "; }
+			if ( token.flags & SKMParsedToken::Flags::QuotedString )		{ flagStr += "QuotedString "; }
+			if ( token.flags & SKMParsedToken::Flags::Identifier )			{ flagStr += "Identifier "; }
+
+			// Do some debug output.
+			Com_DPrintf("   token: {%s}, flags: {%s} \n", token.value.c_str(), flagStr.c_str() );
+		}
+		// Debug output.
+		Com_DPrintf("}\n");
+	}
+	
 	// Debug output for testing tokenizing.
 	Com_DPrintf("===================================================\n");
 
